4752edf03b51d48daf86bf868476abb73d152988
dirkarnez <smalldirkalex@gmail.com>
2019-08-01 13:01:03 +0800

    - init

---
+++ PythonOnDotNet/Program.cs
@@ -0,0 +1,22 @@
+﻿using Python.Runtime;
+using System;
+
+namespace PythonOnDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Py.GIL())
+            {
+                // https://www.python.org/ftp/python/3.5.0/python-3.5.0-embed-win32.zip
+                dynamic client = Py.Import("http.client");
+                dynamic conn = client.HTTPSConnection("www.python.org");
+                conn.request("GET", "/static/favicon.ico");
+                Console.WriteLine(conn.getresponse().status);
+                conn.close();
+                Console.ReadLine();
+            }
+        }
+    }
+}
